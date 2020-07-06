761cd4f514d30be291ba083d353bd3c7d798821f
Gina Joue <joue@nin.uke.uni-hamburg.de>
2020-07-06 02:17:06 +0200

    clean up

--- rename_addSubfolder2Nm.sh
+++
@@ -1,1 +0,0 @@
-for d in fold*/sub*/; do file=`ls $d*.txt`; f=${d/\/*/}; echo $f; echo $file; outfile=${file/.txt/.$f.txt}; echo $outfile; mv $file $outfile; done
--- rename_recurs.sh
+++
@@ -1,2 +0,0 @@
-find . -type f -name "*.txt" -exec sh -c 'mv "$0" "${0%.*}.JUL.ErC_R.txt"' {} \;
-
